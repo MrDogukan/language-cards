ebf2bc55f89c4164c941b519a73aa7a76ad9b940
aliseslikaya <aliseslikaya@gmail.com>
2022-06-29 22:15:39 +0300

    first commit

---
+++ src/components/card/Card.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Card.css";
+import { languages } from "../../helpers/data";
+import Item from "../item/Item";
+
+const Card = () => {
+  return (
+    <div className="cards-area-container">
+      <div className="bars"></div>
+      <h1 className="language-title">Languages</h1>
+      <div className="cards-container">
+        {languages.map((item, index) => {
+          return <Item card={item} key={index} />;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Card;
---
+++ src/components/header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+import reactLogo from "../../assets/react.svg";
+
+const Header = () => {
+  return (
+    <div className="logo-container">
+      <img className="react-logo" src={reactLogo} alt="react-logo" />
+    </div>
+  );
+};
+
+export default Header;
